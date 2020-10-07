bbe90c82952604d74e31fff80ef0bbd29a729563
Jason Lai <jason@192-168-1-15.tpgi.com.au>
2020-10-07 17:34:44 +1100

    Added speech-text conversion

--- test.py
+++ test.py
@@ -2,7 +2,52 @@
 # SUMO HACKATHON 2020
 # Author: Jason Lai
 
-import cv2
-import imutils
+import speech_recognition as sr
+from gtts import gTTS
+import os
+language = 'en'
+from playsound import playsound
+# operating mode
+ASK_ITEM = 1
+GIVE_DIRECTION = 2
+IS_CORRECT_ITEM = 3
 
-print("Hello, World!")
+# directions
+LEFT = 1
+RIGHT = 2
+
+print("Hello")
+
+def speech2text(mode, optarg1, optarg2):
+    r = sr.Recognizer()
+
+    if mode == ASK_ITEM:
+        test = sr.AudioFile('test.wav')
+        with test as source:
+            audio = r.record(source)
+        request = "You requested " + r.recognize_google(audio)
+        print(request)
+        output = gTTS(text=request, lang=language, slow=False)
+    elif mode == GIVE_DIRECTION:
+        # optarg1 is the correct direction, optarg2 is the aisle
+        if optarg1 == LEFT:
+            direction = "turn left into aisle"
+        else:
+            direction = "turn right into aisle"
+        direction = direction + str(optarg2)
+        print(direction)
+        output = gTTS(text=direction, lang=language, slow=False)
+    elif mode == IS_CORRECT_ITEM:
+        # optarg1 is either YES or NO
+        if optarg1 == YES:
+            answer = "This is the correct item"
+        else:
+            answer = "This is not the correct item"
+        print(answer)
+        output = gTTS(text=answer, lang=language, slow=False)
+    else:
+        print("invalid mode")
+    
+    output.save("test.mp3")
+
+speech2text(ASK_ITEM, 0, 0)
