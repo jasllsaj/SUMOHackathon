7e5e6a2054495aeca1c646dcb27adaf62a159182
Jason Lai <jason@192-168-1-15.tpgi.com.au>
2020-10-07 17:45:29 +1100

    modified speech-text converter

--- test.py
+++ test.py
@@ -27,22 +27,22 @@ def speech2text(mode, optarg1, optarg2):
             audio = r.record(source)
         request = "You requested " + r.recognize_google(audio)
         print(request)
-        output = gTTS(text=request, lang=language, slow=False)
+        output = gTTS(text=request, lang=language, slow=True)
     elif mode == GIVE_DIRECTION:
         # optarg1 is the correct direction, optarg2 is the aisle
         if optarg1 == LEFT:
-            direction = "turn left into aisle"
+            direction = "turn left into aisle. "
         else:
-            direction = "turn right into aisle"
+            direction = "turn right into aisle. "
         direction = direction + str(optarg2)
         print(direction)
-        output = gTTS(text=direction, lang=language, slow=False)
+        output = gTTS(text=direction, lang=language, slow=True)
     elif mode == IS_CORRECT_ITEM:
-        # optarg1 is either YES or NO
-        if optarg1 == YES:
+        # optarg1 is whether the item is correct
+        if optarg1 == True:
             answer = "This is the correct item"
         else:
-            answer = "This is not the correct item"
+            answer = "This is an incorrect item"
         print(answer)
         output = gTTS(text=answer, lang=language, slow=False)
     else:
@@ -50,4 +50,4 @@ def speech2text(mode, optarg1, optarg2):
     
     output.save("test.mp3")
 
-speech2text(ASK_ITEM, 0, 0)
+speech2text(IS_CORRECT_ITEM, True, 0)
